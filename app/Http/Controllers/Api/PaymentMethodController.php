b09548cc12ade0ac3b588cdd99e7aee3bd0e1755
joykeerz <wisnu.mukti10@gmail.com>
2024-02-26 14:34:02 +0700

    fixed bugs

--- app/Http/Controllers/Api/PaymentMethodController.php
+++ app/Http/Controllers/Api/PaymentMethodController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\PosPayment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Validator;
 
 class PaymentMethodController extends Controller
@@ -37,13 +38,15 @@ class PaymentMethodController extends Controller
 
         $validator = Validator::make([
             'method_name' => 'required|string',
+            'payment_type' => 'required|string',
             'payment_info' => 'required|string',
         ], [
             'method_name.required' => 'method name is required',
+            'payment_type.required' => 'Payment type is required',
             'payment_info.required' => 'Payment info is required',
         ]);
 
-        if($validator->fails()) {
+        if ($validator->fails()) {
             return response()->json([
                 'error' => $validator->errors()
             ], 400);
@@ -52,7 +55,23 @@ class PaymentMethodController extends Controller
         $paymentMethod = new PosPayment();
         $paymentMethod->profile_id = Auth::user()->posProfile->id;
         $paymentMethod->payment_method = $request->method_name;
+        $paymentMethod->payment_type = $request->payment_type;
+        $filepath = '';
+        if ($request->hasFile('payment_file')) {
+            $url = env('API_DASHBOARD_URL') . '/mobile/receive-payment-method-image';
+            $image = $request->file('payment_file');
+            $fileName = 'image_' . time() . '.' . $image->getClientOriginalExtension();
+            $imageContent = file_get_contents($image->getRealPath());
+            $response = Http::attach(
+                'payment_file',
+                $imageContent,
+                $fileName
+            )->post($url);
+            $responseData = $response->json();
+            $filePath = $responseData['path'];
+        }
         $paymentMethod->payment_info = $request->payment_info;
+        $paymentMethod->payment_file = $filePath ?? 'images/pos/payment/methods/default.png';
         $paymentMethod->save();
 
         return response()->json($paymentMethod, 201);
