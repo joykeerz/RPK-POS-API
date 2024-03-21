730b73fc2a402b5032ce99b27f3f7f823539c221
joykeerz <wisnu.mukti10@gmail.com>
2024-03-21 11:57:01 +0700

    bugfixes

--- app/Http/Controllers/Api/PrinterController.php
+++ app/Http/Controllers/Api/PrinterController.php
@@ -25,7 +25,7 @@ class PrinterController extends Controller
     public function show(string $id)
     {
         $printer = PosPrinter::where('id', $id)->first();
-        if (empty($printers) || count($printers) < 1) {
+        if (empty($printers) || count($printers) == 0) {
             return response()->json([
                 'error' => "printer not found"
             ], 404);
